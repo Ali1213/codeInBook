7ea732c05768a087f92d050fed283dce7805917f
Ali1213 <yusili2000@163.com>
2018-05-20 01:37:22 +0800

    add:

---
+++ go语言圣经/2.chapter/10.printInts.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	i := []int{1, 2, 3, 4, 5}
+	fmt.Println(intsToString(i))
+}
+
+func intsToString(values []int) string {
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+
+	for i, v := range values {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		fmt.Fprintf(&buf, "%d", v)
+	}
+	buf.WriteByte(']')
+	return buf.String()
+}
